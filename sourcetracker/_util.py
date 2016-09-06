f31dc960b97ad4d3196ccfc6dd52327f862b1c6d
Greg Caporaso <gregcaporaso@gmail.com>
2016-09-06 16:14:15 -0700

    DOC: adds docstring

--- sourcetracker/_util.py
+++ sourcetracker/_util.py
@@ -12,6 +12,20 @@ import pandas as pd
 
 
 def parse_sample_metadata(f):
+    """ Parse QIIME 1-like sample metadata file
+
+    Parameters
+    ----------
+    f : file handle
+        The sample metadata to be parse.
+
+    Returns
+    -------
+    pd.DataFrame
+        Sample metadata where index is sample ids and columns are metadata
+        categories.
+
+    """
     sample_metadata = pd.read_csv(f, sep='\t', dtype=object)
     sample_metadata.set_index(sample_metadata.columns[0], drop=True,
                               append=False, inplace=True)
