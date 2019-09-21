63e23f141fc1b91a9edf2289b2cb5c2d2151116e
cameronmartino <cameronmartino@gmail.com>
2019-09-21 14:52:27 -0700

    fixing travis issues

--- setup.py
+++ setup.py
@@ -15,28 +15,6 @@ from setuptools import find_packages, setup
 
 __version__ = '2.0.1-dev'
 
-classes = """
-    Development Status :: 1 - Planning
-    Intended Audience :: Science/Research
-    Natural Language :: English
-    Operating System :: MacOS :: MacOS X
-    Operating System :: POSIX
-    Operating System :: Unix
-    Programming Language :: Python
-    Programming Language :: Python :: 3.5
-    Topic :: Scientific/Engineering
-    Topic :: Scientific/Engineering :: Bio-Informatics
-"""
-classifiers = [s.strip() for s in classes.split('\n') if s]
-
-description = "Python implementation of the SourceTracker R package."
-
-standalone = ['sourcetracker2=sourcetracker._cli.gibbs:gibbs']
-q2cmds = ['q2-sourcetracker2=sourcetracker._q2.plugin_setup:plugin']
-
-with open('README.md') as f:
-    long_description = f.read()
-
 # Dealing with Cython
 USE_CYTHON = os.environ.get('USE_CYTHON', False)
 ext = '.pyx' if USE_CYTHON else '.c'
@@ -73,6 +51,33 @@ class CustomEggInfoCommand(egg_info):
 extensions = [
 ]
 
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+classes = """
+    Development Status :: 1 - Planning
+    Intended Audience :: Science/Research
+    Natural Language :: English
+    Operating System :: MacOS :: MacOS X
+    Operating System :: POSIX
+    Operating System :: Unix
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Bio-Informatics
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
+description = "Python implementation of the SourceTracker R package."
+
+standalone = ['sourcetracker2=sourcetracker._cli.gibbs:gibbs']
+q2cmds = ['q2-sourcetracker2=sourcetracker._q2.plugin_setup:plugin']
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
     name='sourcetracker',
     version=__version__,
@@ -85,11 +90,19 @@ setup(
     maintainer='Will Van Treuren',
     maintainer_email='will@biota.com',
     url='http://www.biota.com',
-    test_suite='nose.collector',
     packages=find_packages(),
-    install_requires=['numpy', 'h5py', 'biom-format >= 2.1.5, < 2.2.0',
-                      'click', 'scikit-bio', 'pandas', 'scipy >= 0.18.0',
-                      'seaborn'],
+    ext_modules=extensions,
+    install_requires=[
+          'numpy',
+          'click',
+          'pandas',
+          'scipy',
+          'nose',
+          'scikit-learn',
+          'scikit-bio',
+          'biom-format',
+          'h5py',
+          'seaborn'],
     classifiers=classifiers,
     package_data={'sourcetracker/_q2': ['citations.bib']},
     entry_points={'qiime2.plugins': q2cmds,
