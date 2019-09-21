9fb8b7e87a064bafed0e6ebc4a6137f29385fba7
cameronmartino <cameronmartino@gmail.com>
2019-09-20 17:17:13 -0700

    fix numpy pip issues with q2

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from setuptools.command.egg_info import egg_info
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import os
 from setuptools import find_packages, setup
 
 __version__ = '2.0.1-dev'
@@ -33,6 +37,42 @@ q2cmds = ['q2-sourcetracker2=sourcetracker._q2.plugin_setup:plugin']
 with open('README.md') as f:
     long_description = f.read()
 
+# Dealing with Cython
+USE_CYTHON = os.environ.get('USE_CYTHON', False)
+ext = '.pyx' if USE_CYTHON else '.c'
+
+
+# bootstrap numpy intall
+# https://stackoverflow.com/questions/51546255/
+# python-package-setup-setup-py-with-customisation
+# -to-handle-wrapped-fortran
+def custom_command():
+    import sys
+    if sys.platform in ['darwin', 'linux']:
+        os.system('pip install numpy')
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        custom_command()
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        custom_command()
+
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+        custom_command()
+
+
+extensions = [
+]
+
 setup(
     name='sourcetracker',
     version=__version__,
@@ -53,4 +93,8 @@ setup(
     classifiers=classifiers,
     package_data={'sourcetracker/_q2': ['citations.bib']},
     entry_points={'qiime2.plugins': q2cmds,
-                  'console_scripts': standalone})
+                  'console_scripts': standalone},
+    cmdclass={'install': CustomInstallCommand,
+              'develop': CustomDevelopCommand,
+              'egg_info': CustomEggInfoCommand},
+    zip_safe=False)
