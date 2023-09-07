52bafa1237486b2ddcb84d0df9b22d5c5ab718c1
Chloe Herman <60228108+cherman2@users.noreply.github.com>
2023-09-07 10:25:52 -0700

    Update sourcetracker/_cli/gibbs.py

--- sourcetracker/_cli/gibbs.py
+++ sourcetracker/_cli/gibbs.py
@@ -187,7 +187,7 @@ def gibbs(table_fp: Table,
     if diagnostics:
         os.mkdir(output_dir+'diagnostics')
         data = np.load('envcounts.npy', allow_pickle=True)
-        sink_ids = np.load('sink_ids.npy',allow_pickle=True)
+        sink_ids = np.load('sink_ids.npy', allow_pickle=True)
         source_ids = np.load('source_ids.npy',allow_pickle=True)
         file_path = output_dir+'diagnostics'
         
