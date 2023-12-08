321f6e717935ba19f07e8088348e393e3db2f464
andy6a <128522580+andy6a@users.noreply.github.com>
2023-12-08 14:01:21 -0800

    Update gibbs.py
    Add diagnostics addition to gibbs.py

--- sourcetracker/_cli/gibbs.py
+++ sourcetracker/_cli/gibbs.py
@@ -13,7 +13,10 @@ from __future__ import division
 import os
 import click
 import pandas as pd
+import numpy as np
 from biom import Table, load_table
+from matplotlib import pyplot as plt
+
 from sourcetracker._cli import cli
 from sourcetracker._gibbs import gibbs_helper
 from sourcetracker._plot import plot_heatmap
@@ -115,11 +118,10 @@ from sourcetracker._gibbs_defaults import (DEFAULT_ALPH1, DEFAULT_ALPH2,
 @click.option('--source_category_column', required=False, default=DEFAULT_CAT,
               type=click.STRING, show_default=True,
               help=DESC_CAT)
-
-##(added click option for stats function)
+# Stats functions for diagnostics
 @click.option('--diagnostics', required=False, default=False, is_flag=True,
               show_default=True)
-@click.option('--limit', required=False, default=0.05, type=click.FLOAT, 
+@click.option('--limit', required=False, default=0.05, type=click.FLOAT,
               show_default=True)
 
 def gibbs(table_fp: Table,
@@ -142,7 +144,7 @@ def gibbs(table_fp: Table,
           source_column_value: str,
           sink_column_value: str,
           source_category_column: str,
-          diagnostics: bool, 
+          diagnostics: bool,
           limit: float):
     '''Gibb's sampler for Bayesian estimation of microbial sample sources.
 
@@ -183,59 +185,60 @@ def gibbs(table_fp: Table,
     # Plot contributions.
     fig, ax = plot_heatmap(mpm.T)
     fig.savefig(os.path.join(output_dir, 'mixing_proportions.pdf'), dpi=300)
-          
+
+    # Diagnostics code, should work, if not this code was prewritten so this will be a bit of a mess
     if diagnostics:
-        os.mkdir(output_dir+'diagnostics')
+        os.mkdir(output_dir + 'diagnostics')
         data = np.load('envcounts.npy', allow_pickle=True)
         sink_ids = np.load('sink_ids.npy', allow_pickle=True)
         source_ids = np.load('source_ids.npy', allow_pickle=True)
-        file_path = output_dir+'diagnostics'
-        
+        file_path = output_dir + 'diagnostics'
+
         source_ids = np.append(source_ids, ['unknown'])
-        df = pandas.DataFrame(source_ids)
+        df = pd.DataFrame(source_ids)
         sink_index = -1
         for array in data:
             sink_df = []
             sink_index += 1
             sink_id = sink_ids[sink_index]
             source_index = -1
-        
+
             for sources in source_ids:
                 source_index += 1
-                source_array = array[:,source_index]
+                source_array = array[:, source_index]
                 split_array = np.array_split(source_array, draws_per_restart)
-                plt.figure(figsize=(8,6), dpi=300), plt.title(sink_id, fontsize = (16))
-                
+                plt.figure(figsize=(8, 6), dpi=300), plt.title(sink_id, fontsize=(16))
+
                 flagged = []
                 for splits in split_array:
                     data_sum = np.cumsum(splits)
                     restart_num = np.size(data_sum)
                     vector = np.linspace(1, restart_num, restart_num)
                     rolling = np.true_divide(data_sum, vector)
-                    
-                    scalar = [(endpoint*alpha1) for endpoint in rolling]
+
+                    scalar = [(endpoint * alpha1) for endpoint in rolling]
                     line_average = np.average(scalar)
                     line_average = np.round(line_average, decimals=4)
                     flagged.append(line_average)
                     plt.plot(scalar, label=line_average), plt.legend(), plt.ylabel(sources, fontsize=(16))
-                    
+
                 absolutes = [abs(chains) for chains in flagged]
-                difference = (max(absolutes)-min(absolutes))
+                difference = (max(absolutes) - min(absolutes))
                 sink_df.append(difference)
-                
+
                 if difference >= limit:
-                    file_name = sink_id+'_'+sources+'.png'
+                    file_name = sink_id + '_' + sources + '.png'
                     plt.savefig(os.path.join(file_path, file_name))
                 else:
                     pass
                 plt.close()
-        
-            sink_df = pandas.DataFrame(sink_df)
+
+            sink_df = pd.DataFrame(sink_df)
             df[sink_id] = sink_df
             df.columns.values[0] = ''
             df.set_index('').T
-            df.to_csv(file_path+'/'+'table.txt', sep='\t', index=False)
-            
+            df.to_csv(file_path + '/' + 'table.txt', sep='\t', index=False)
+
     os.remove('envcounts.npy')
     os.remove('sink_ids.npy')
     os.remove('source_ids.npy')
